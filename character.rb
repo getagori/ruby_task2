9f1823514cc38515f0715c4c34a41938a7bedf4e
getagori <57044519+getagori@users.noreply.github.com>
2020-02-18 12:26:34 +0900

    クラスをStatusからCharacterに変更

--- character.rb
+++ character.rb
@@ -1,7 +1,7 @@
 class Character
 #原則，継承はis_a の関係.monster is a statusは命名として違和感がある。
     attr_reader :name, :offence, :defence
-    attr_accessor :hp 
+    attr_accessor :hp
         def initialize(**params)
             @name = params[:name]
             @hp = params[:hp]
